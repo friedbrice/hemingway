360ad98087ff39d30fd5d708b7daa93899630490
Daniel Brice <danielbrice@gmail.com>
2015-01-29 17:51:58 -0600

    primes is working, though not optimal.

--- primes/primes.hs
+++ primes/primes.hs
@@ -3,10 +3,9 @@ import System.Environment (getArgs)
 isPrime :: Integer -> Bool
 isPrime n = null . dropWhile (0 /=) $ rems
   where
-  rems = map (rem n) $ [1..k]
-  k = ceiling . sqrt $ n
+  rems = map (rem n) [2..k]
+  k = ceiling . sqrt . fromInteger $ n
 
 
---main :: IO()
---main = mapM_ print . (\n -> filter isPrime [1..n]) . read . head =<< getArgs
-
+main :: IO()
+main = mapM_ print . filter isPrime . enumFromTo 1 . read . head =<< getArgs
