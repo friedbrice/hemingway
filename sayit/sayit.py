796540d648eb733f8acca38e8df04c961f5822e8
Daniel Brice <danielbrice@gmail.com>
2015-05-25 11:02:19 -0700

    added extra functionality (get it?) to the python solution.

--- sayit/sayit.py
+++ sayit/sayit.py
@@ -1,24 +1,27 @@
-### Prototype
+### As a function that returns itself
 
-# words = []
+words = []
 
-# def sayIt( x = None ):
-# 	global words
-# 	if x is None:
-# 		message = ' '.join(words)
-# 		print(message)
-# 	else:
-# 		words = words + [x]
+def sayIt( x = None ):
+    global words
+    if x is None:
+        message = ' '.join(words)
+        print(message)
+        words = []
+    else:
+        words = words + [x]
+        return sayIt
 
-### Real Thing
+### As a class
 
 class Words:
-	def __init__(self):
-		self.words = []
-	def _(self, x = None):
-		if x is None:
-			message = ' '.join(self.words)
-			print(message)
-		else:
-			self.words = self.words + [x]
-		return self
+    def __init__(self):
+        self.words = []
+    def _(self, x = None):
+        if x is None:
+            message = ' '.join(self.words)
+            print(message)
+            self.words = []
+        else:
+            self.words = self.words + [x]
+        return self
