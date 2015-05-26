04197fb1ead90bfc34c285b0115f9f112c1dbf9d
Daniel Brice <danielbrice@gmail.com>
2015-05-26 16:09:54 -0700

    was hoping fibonacci3.hs would get faster, but it got slower.

--- fibonacci/fibonacci3.hs
+++ fibonacci/fibonacci3.hs
@@ -1,14 +1,15 @@
 import System.Environment (getArgs)
 
 fib :: Integer -> Integer
+fib 0 = 0
 fib 1 = 1
-fib 2 = 1
-fib 3 = 2
-fib n = fib(n-1) + fib(n-2)
+fib n | n >= 2 = fib(n-1) + fib(n-2)
+
+fibs = map fib [1..]
 
 main :: IO ()
 main = do
     args <- getArgs
     let n = read . head $ args
-    let fibs = map fib [1..n]
-    print fibs
+    let fibs' = take n fibs
+    print fibs'
