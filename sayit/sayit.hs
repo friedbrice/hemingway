cd53d619e36a1b6bcb9caebac0a3acdf63f2a209
Daniel Brice <danielbrice@gmail.com>
2015-08-24 09:11:54 -0700

    quite possibly the dumbest haskell program ever written

--- sayit/sayit.hs
+++ sayit/sayit.hs
@@ -1,22 +1,21 @@
---import Control.Monad.State
+-- this is quite possibly the dumbest haskell program ever written
 
---emptyStack :: State [String] String
---emptyStack = state $ \s -> (unwords . reverse $ s, [])
+split :: (Char -> Bool) -> String -> [String]
+-- ^ thanks to Steve, http://stackoverflow.com/questions/4978578/how-to-split-a-string-in-haskell
+split p s =  case dropWhile p s of
+                      "" -> []
+                      s' -> w : split p s''
+                            where (w, s'') = break p s'
 
---appendStack :: String -> State [String] ()
---appendStack x = state $ \s -> ((), x:s)
+remove :: Eq a => a -> [a] -> [a]
+remove y xs = filter (/= y) xs
 
---printStack :: State [String] (IO ())
---printStack = do
---    a <- emptyStack
---    return $ putStrLn a
+main' :: String -> IO ()
+main' = print . unwords . split (== '(') . remove ')' . remove '\''
 
---main :: IO ()
---main = do
---    l <- getLine
---    if null l
---        then do
---            runState printStack
---        else do
---            return $ appendStack l
---            main
+main :: IO ()
+main = do
+    input <- getLine
+    if ((last input) /= ')' || (last . init $ input) /= '(')
+        then print ""
+        else main' input
