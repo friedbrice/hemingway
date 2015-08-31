163edb921fb28babbd5cb2e17370b1cf586a69c5
Daniel Brice <danielbrice@gmail.com>
2015-08-30 23:56:48 -0700

    still working on this

--- sayit/sayit.hs
+++ sayit/sayit.hs
@@ -1,25 +1,9 @@
--- this is quite possibly the dumbest haskell program ever written
+import Data.Either
 
-split :: (Char -> Bool) -> String -> [String]
--- ^ thanks to Steve, http://stackoverflow.com/questions/4978578/how-to-split-a-string-in-haskell
-split p s =  case dropWhile p s of
-                      "" -> []
-                      s' -> w : split p s''
-                            where (w, s'') = break p s'
+data Saying = State String String
+data Ins = Either String ()
+data Outs = Either Saying (IO ())
 
-remove :: Eq a => a -> [a] -> [a]
-remove y xs = filter (/= y) xs
-
-main' :: String -> IO ()
-main' = print . unwords . split (== '(') . remove ')' . remove '\''
-
-main :: IO ()
-main = do
-    input <- getLine
-    if ((last input) /= ')' || (last . init $ input) /= '(')
-        then print ""
-        else main' input
-
--- will print "" if fed "()" or "('x')('y')...('z')"
--- will print "x y ... z" if fed "('x')('y')...('z')()"
--- accepts improperly formatted input and is unpredictable with it
+sayIt :: Ins -> Outs
+sayIt x = case x of Left s   -> 
+                    Right () -> 
