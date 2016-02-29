bca70421e09164569e103daee24d842bba1ed709
Daniel Brice <danielbrice@gmail.com>
2016-02-28 22:06:32 -0800

    making it all professional like

---
+++ fibonacci/Fibonacci.py
@@ -0,0 +1,34 @@
+import sys
+
+# Recursive
+def recFibs(n):
+  def fib(k):
+    if k <= 1:
+      return k
+    else:
+      return fib(k - 1) + fib(k - 2)
+  fibs = list(map(fib, range(0, n + 1)))
+  return fibs
+
+# Memoized
+def fastFibs(n):
+  fibs = [0, 1]
+  if n <= 1:
+    return fibs
+  else:
+    for k in range(2, n + 1):
+      fibs.append(fibs[k - 1] + fibs[k - 2])
+    return fibs
+
+# Console IO
+method = sys.argv[1]
+n = int(sys.argv[2])
+if method == "rec":
+  fibs = recFibs(n)
+elif method == "fast":
+  fibs = fastFibs(n)
+else:
+  print("Please specify \"rec\" or \"fast\".")
+  exit(1)
+print(fibs)
+exit()
