629dd7cadaa1351355fa84c22b6fb3751d0024ac
Daniel P. Brice <danielbrice@gmail.com>
2016-03-04 10:27:44 -0800

    Basic control flow down, just need state.

--- SayIt/SayIt.hs
+++ SayIt/SayIt.hs
@@ -5,5 +5,8 @@
 -- A work in progress
 ----
 
-SayIt :: String -> IO ()
-SayIt = undefined
+data Codom = IO () | String -> Codom
+
+SayIt :: Maybe String -> Codom
+SayIt Nothing = print "Nothing"
+SayIt Just s0 = (\s1 -> SayIt(s0 ++ s1))
