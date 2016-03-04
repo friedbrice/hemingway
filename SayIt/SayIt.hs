1bb25ee6a9d39f8cbc94e8c9fc4e499623a1f42a
Daniel P. Brice <danielbrice@gmail.com>
2016-03-04 10:34:25 -0800

    can't check in ghci b/c at work <.<

--- SayIt/SayIt.hs
+++ SayIt/SayIt.hs
@@ -5,8 +5,8 @@
 -- A work in progress
 ----
 
-data Codom = IO () | (Maybe String -> Codom)
+data Codom = CodomIO (IO ()) | CodomFunc (Maybe String -> Codom)
 
 SayIt :: Maybe String -> Codom
-SayIt Nothing   = print "Nothing"
-SayIt (Just s0) = \input -> case input of {Nothing -> print s0;  Just s1 -> SayIt(s0 ++ s1)}
+SayIt Nothing   = CodomIO (print "Nothing")
+SayIt (Just s0) = CodomFunc (\input -> case input of {Nothing -> print s0;  Just s1 -> SayIt(s0 ++ s1)})
