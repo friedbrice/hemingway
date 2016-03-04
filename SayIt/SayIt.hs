6f7c2087668dd269e76c7c7cfcb6d6a520b44620
Daniel Brice <danielbrice@gmail.com>
2016-03-04 12:38:43 -0800

    doesn't compile

--- SayIt/SayIt.hs
+++ SayIt/SayIt.hs
@@ -1,19 +1,21 @@
 ----
 -- SayIt.hs
 -- by daniel
--- 
+--
 -- A work in progress
 ----
 
 data Codom = CodomIO (IO ()) | CodomFunc (Maybe String -> Codom)
 
 sayIt :: Maybe String -> Codom
-sayIt Nothing   = CodomIO (print "Nothing")
-sayIt (Just s0) = CodomFunc (\input -> case input of {Nothing -> CodomIO (print s0);  Just s1 -> CodomFunc (SayIt(s0 ++ s1)}))
--- this has the basic control structure and state information. we just need a way to evaluate a CodomFunc
+sayIt x = case x of
+  Nothing -> CodomIO (print "Nothing")
+  Just s0 -> CodomFunc f
+    where
+      f y = case y of
+        Nothing -> CodomIO (print s0)
+        Just s1 -> CodomFunc (sayIt(s0 ++ " " ++ s1))
 
-evalCodomFunc :: Codom -> Maybe String -> Codom
-evalCodomFunc (CodomFunc f) Nothing   = CodomIO (f Nothing)
-evalCodomFunc (CodomFunc f) (Just s0) = CodomFunc (f (Just s0))
-evalCodomFunc (CodomIO a)   Nothing   = CodomIO a
-evalCodomFunc (CodomIO a)   (Just s0) = error "This shouldn't happen..."
+--evalCodomFunc :: Codom -> Maybe String -> Codom
+--evalCodomFunc (CodomFunc f) Nothing   = CodomIO (f Nothing)
+--evalCodomFunc (CodomFunc f) (Just s0) = CodomFunc (f (Just s0))
