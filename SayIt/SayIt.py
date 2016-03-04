df42426a0b79027e0cfcc58e7aa04f44b93fb1f0
Daniel P. Brice <danielbrice@gmail.com>
2016-03-04 10:11:35 -0800

    added comments

--- SayIt/SayIt.py
+++ SayIt/SayIt.py
@@ -1,4 +1,21 @@
-### as a function that modifies a global
+###
+# SayIt.py
+# Created by daniel.
+#
+# SayIt requires we write a chainable function that takes either a string or
+# no input. Upon taking a string, the chainable function must modify state
+# and return itself. Upon taking no input, the chainable function must act on
+# the current state and then clear the state.
+#
+# I provide four approaches to the solution:
+# - As a function that modified a global
+# - As a class with a single method that return the instance
+# - As a closure, implemented by a wrapper function
+# - As a closure, implemented by returning a lambda (and the magic of default arguments)
+# The closure-by-wrapper implementation corresponds most closely with the design objective.
+###
+
+# as a function that modifies a global
 words = []
 
 def sayIt(x = None):
@@ -11,7 +28,7 @@ def sayIt(x = None):
         words = words + [x]
         return sayIt
 
-### as a class with one property and one method
+# as a class with one property and one method
 class objectSayIt:
     def __init__(self):
         self.words = []
@@ -24,7 +41,7 @@ class objectSayIt:
             self.words = self.words + [x]
             return self
 
-### as a closure, implemented by a wrapper function
+# as a closure, implemented by a wrapper function
 def wrapperSayIt(word = None):
     acc = []
 
@@ -38,7 +55,7 @@ def wrapperSayIt(word = None):
             return fn
     return fn(word)
 
-### as a closure, implemented by returning a lambda
+# as a closure, implemented by returning a lambda
 def closureSayIt(x = None, acc = []):
     if x is None:
         message = ' '.join(acc)
