17ce511c0351d2d86e064fc135483655ca0d127b
Daniel P. Brice <danielbrice@gmail.com>
2016-03-04 10:44:30 -0800

    It seems like i'm in the infinite Modus Ponens descent... <.<;

--- SayIt/SayIt.hs
+++ SayIt/SayIt.hs
@@ -12,6 +12,8 @@ sayIt Nothing   = CodomIO (print "Nothing")
 sayIt (Just s0) = CodomFunc (\input -> case input of {Nothing -> CodomIO (print s0);  Just s1 -> CodomFunc (SayIt(s0 ++ s1)}))
 -- this has the basic control structure and state information. we just need a way to evaluate a CodomFunc
 
-evalCodomFunc :: CodomFunc -> Maybe String -> Codom
+evalCodomFunc :: Codom -> Maybe String -> Codom
 evalCodomFunc (CodomFunc f) Nothing   = CodomIO (f Nothing)
 evalCodomFunc (CodomFunc f) (Just s0) = CodomFunc (f (Just s0))
+evalCodomFunc (CodomIO a)   Nothing   = CodomIO a
+evalCodomFunc (CodomIO a)   (Just s0) = error "This shouldn't happen..."
