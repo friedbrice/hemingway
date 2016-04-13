dc6eaac08a718b604839557fb520b1df2dadded5
Daniel Brice <danielbrice@gmail.com>
2016-04-12 19:28:32 -0700

    have a working snippet. not very good

--- SayIt/SayIt.hs
+++ SayIt/SayIt.hs
@@ -5,17 +5,16 @@
 -- A work in progress
 ----
 
-data Codom = CodomIO (IO ()) | CodomFunc (Maybe String -> Codom)
+sayIt :: String -> IO () -> IO ()
+sayIt string = \action -> do
+  putStr string
+  action
 
-sayIt :: Maybe String -> Codom
-sayIt x = case x of
-  Nothing -> CodomIO (print "Nothing")
-  Just s0 -> CodomFunc f
-    where
-      f y = case y of
-        Nothing -> CodomIO (print s0)
-        Just s1 -> CodomFunc (sayIt(s0 ++ " " ++ s1))
-
---evalCodomFunc :: Codom -> Maybe String -> Codom
---evalCodomFunc (CodomFunc f) Nothing   = CodomIO (f Nothing)
---evalCodomFunc (CodomFunc f) (Just s0) = CodomFunc (f (Just s0))
+main :: IO ()
+main
+  = sayIt "Hi"
+  . sayIt " "
+  . sayIt "there"
+  . sayIt "!"
+  . sayIt "\n"
+  $ return ()
