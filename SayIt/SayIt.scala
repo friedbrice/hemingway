9aa8eafd4759e96532e52a9dd70d4cfdd51e7e2f
Daniel Brice <dbrice@cj.com>
2016-07-24 20:37:07 -0700

    trying to make these runable from command line

---
+++ SayIt/SayIt.scala
@@ -0,0 +1,47 @@
+/**
+ * Factorial.scala
+ * by Daniel Brice
+ * Invoke as `scala Factorial <num>`
+ * Prints <num> factorial
+ */
+
+object SayIt {
+
+  sealed trait Sayable extends Function1[Option[String], Sayable]
+
+  case object SayableTerminate extends Sayable {
+    def apply(optStr : Option[String]) : Sayable = SayableTerminate
+  }
+
+  case class SayableAccumulate(f: Option[String] => Sayable) extends Sayable {
+    def apply(optStr : Option[String]): Sayable = f(optStr)
+  }
+
+  def sayIt
+    : Option[String] => Sayable
+    = optStr         => {
+
+      var acc = ""
+
+      def helper
+        : Option[String] => Sayable
+        = {
+            case None => {
+              println(acc)
+              SayableTerminate
+            }
+            case Some(str) => {
+              acc = acc + " " + str
+              SayableAccumulate(helper)
+            }
+        }
+
+      helper(optStr)
+    }
+
+  def main
+    : Array[String] => Unit
+    = args          => {
+      sayIt(Some("Hi"))(Some("There"))(Some("World"))(None)
+    }
+}
